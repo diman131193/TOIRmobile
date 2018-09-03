b30993ba70f13e6556e10dc62dd8ae969eaa9c76
Александр Ульянов <a.ulyanov@corp.metholding.com>
2018-09-03 18:20:08 +0300

    Added Joystick Model rotation script

---
+++ Assets/Scripts/ModelRotation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelRotation : MonoBehaviour
+{
+    [Header("Options")]
+    public float rotationSpeed;
+    public float joystickSensivity;
+
+    [Header("Components")]
+    public Transform Model;
+    public Joystick joystick;
+
+    void Update() {
+        Vector3 moveVector = Vector3.right * joystick.Horizontal + Vector3.up * joystick.Vertical;
+
+        Vector3 rotateDirection;
+
+        if (moveVector.x > joystickSensivity)
+        {
+            Model.RotateAround(Model.position, Vector3.down, rotationSpeed);
+        }
+
+        if (moveVector.x < -joystickSensivity)
+        {
+            Model.RotateAround(Model.position, Vector3.up, rotationSpeed);
+        }
+
+        if (moveVector.y > joystickSensivity)
+        {
+            Model.RotateAround(Model.position, Vector3.right, rotationSpeed);
+        }
+
+        if (moveVector.y < -joystickSensivity)
+        {
+            Model.RotateAround(Model.position, Vector3.left, rotationSpeed);
+        }
+    }
+}
