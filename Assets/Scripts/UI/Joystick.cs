c46a67729803b18dea4af5d17b4d31ef3a5cb0c2
Александр Ульянов <a.ulyanov@corp.metholding.com>
2018-09-03 17:45:11 +0300

    Joystick Prefab Added

---
+++ Assets/Scripts/UI/Joystick.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    [Header("Options")]
+    [Range(0f, 2f)] public float handleLimit = 1f;
+
+    protected Vector2 inputVector = Vector2.zero;
+
+    [Header("Components")]
+    public RectTransform background;
+    public RectTransform handle;
+
+    public float Horizontal { get { return inputVector.x; } }
+    public float Vertical { get { return inputVector.y; } }
+    public Vector2 Direction { get { return new Vector2(Horizontal, Vertical); } }
+
+    Vector2 joystickPosition = Vector2.zero;
+    private Camera cam = new Camera();
+
+    void Start()
+    {
+        joystickPosition = RectTransformUtility.WorldToScreenPoint(cam, background.position);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 direction = eventData.position - joystickPosition;
+        inputVector = (direction.magnitude > background.sizeDelta.x / 2f) ? direction.normalized : direction / (background.sizeDelta.x / 2f);
+        handle.anchoredPosition = (inputVector * background.sizeDelta.x / 2f) * handleLimit;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnDrag(eventData);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        inputVector = Vector2.zero;
+        handle.anchoredPosition = Vector2.zero;
+    }
+}
