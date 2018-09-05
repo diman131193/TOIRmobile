00cc8bb513a5b18af991341a3c71b7ff681bbbaf
Александр Ульянов <a.ulyanov@corp.metholding.com>
2018-09-05 09:53:40 +0300

    tmp

---
+++ Assets/Scripts/Tmp/Model.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Model : MonoBehaviour {
+    public int SelectedId;
+
+	void Start () {
+        Transform[] childArray = GetComponentsInChildren<Transform>();
+
+        for (var i = 1; i < childArray.Length; i++)
+        {
+            childArray[i].gameObject.AddComponent<OnMouseClick>();
+            childArray[i].gameObject.GetComponent<OnMouseClick>().model = this;
+        }
+    }
+}
---
+++ Assets/Scripts/Tmp/OnMouseClick.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnMouseClick: MonoBehaviour {
+    public Model model;
+    bool singleTouch;
+    float doubleClickSpeed = 1.5f;
+    float timeElapsed = 0.0f;
+
+    void OnMouseDown()
+    {
+        if (!singleTouch)
+        {
+                print(" Single Tap");
+                singleTouch = true;
+                if (GetInstanceID() != model.SelectedId)
+                {
+                    foreach (Renderer child in model.GetComponentsInChildren<Renderer>())
+                    {
+                        child.material.color = Color.white;
+                    }
+                    GetComponent<Renderer>().material.color = Color.blue;
+                    model.SelectedId = GetInstanceID();
+                }
+        }
+        else
+        {
+            print("Double Tap");
+            if (GetInstanceID() == model.SelectedId && GetComponent<Animator>())
+            {
+                GetComponent<Animator>().enabled = true;
+            }
+            
+            singleTouch = false;
+            timeElapsed = 0f;
+        }
+    }
+
+    private void Update()
+    {
+        if (singleTouch)
+        {
+            timeElapsed += Time.deltaTime;
+        }
+        if (timeElapsed > doubleClickSpeed)
+        {
+            print("Values Resetted");
+            timeElapsed = 0f;
+            singleTouch = false;
+        }
+    }
+}
