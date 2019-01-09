d6ed83f86f2b21fcbeeac5f7e684f40b8de42956
diman131193 <diman131193@yandex.ru>
2019-01-09 17:25:19 +0300

    09.01.19

--- Assets/Scripts/Behaviour/ModelRotation.cs
+++ Assets/Scripts/Behaviour/ModelRotation.cs
@@ -10,29 +10,27 @@ public class ModelRotation : MonoBehaviour
 
     [Header("Components")]
     public Transform Model;
-    public Joystick leftJoystick;
     public Joystick rightJoystick;
 
     void Update() {
-        Vector3 leftMoveVector = Vector3.right * leftJoystick.Horizontal + Vector3.up * leftJoystick.Vertical;
         Vector3 rightMoveVector = Vector3.right * rightJoystick.Horizontal + Vector3.up * rightJoystick.Vertical;
 
-        if (leftMoveVector.x > joystickSensivity || rightMoveVector.x > joystickSensivity)
+        if (rightMoveVector.x > joystickSensivity)
         {
             Model.RotateAround(Model.position, Vector3.down, rotationSpeed);
         }
 
-        if (leftMoveVector.x < -joystickSensivity || rightMoveVector.x < -joystickSensivity)
+        if (rightMoveVector.x < -joystickSensivity)
         {
             Model.RotateAround(Model.position, Vector3.up, rotationSpeed);
         }
 
-        if (leftMoveVector.y > joystickSensivity || rightMoveVector.y > joystickSensivity)
+        if (rightMoveVector.y > joystickSensivity)
         {
             Model.RotateAround(Model.position, Vector3.right, rotationSpeed);
         }
 
-        if (leftMoveVector.y < -joystickSensivity || rightMoveVector.y < -joystickSensivity)
+        if (rightMoveVector.y < -joystickSensivity)
         {
             Model.RotateAround(Model.position, Vector3.left, rotationSpeed);
         }
