3171c956f04865cd4a5a8eb1d05816345c5462c6
diman131193 <diman131193@yandex.ru>
2019-04-02 16:41:20 +0300

    change text color

---
+++ Assets/Scripts/UI/ChangeText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeText : MonoBehaviour
+{
+    private float colorIntensity = 70.0f;
+    private float colorFix = 200.0f;
+
+    void Update()
+    {
+        this.gameObject.GetComponent<Text>().color = new Color(colorIntensity / 255.0f, colorIntensity / 255.0f, colorFix / 255.0f, 255.0f / 255.0f);
+    }
+
+    public void changeText(float value)
+    {
+        colorIntensity = value;
+    }
+}
